042be1364878bff00b71272e2beecd91c610893f
Darya Kolesnikova <Kdarias.07@gmail.com>
2024-01-29 23:10:13 +0500

    add files

---
+++ base/config.php
@@ -0,0 +1,13 @@
+<?php
+const DB_USER = 'doreius';
+const DB_NAME = 'mvc';
+const DB_HOST = 'localhost';
+const DB_PASSWORD = 'Hermajesty66';
+
+const ADMIN_IDS = [9];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
